bd1c47d2ed6ed668dcd992b691212e84f260f945
pham-peter <phmptr@gmail.com>
2024-05-02 19:38:25 -0400

    Major Update
    -download cv and contact info buttons smoothed out to rectangles
    -icons added and additional social media links
    -colors and animations added
    -projects updated
    -responsiveness improved
    -artwork section overhaul
    -resume updated
    -various bug and unintended interaction fixes
    -cleaned up the quality of the about me cartoon img
    -links updated

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ function toggleMenu()
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function changeIconLI(isHovering) {
+    var icon = document.getElementById('contactLIIcon');
+    if (isHovering) {
+        icon.src = 'linkedin-app-icon.svg'; // This is the hover icon
+    } else {
+        icon.src = 'linkedin-square-icon.svg'; // This is the original icon
+    }
+}
+
+function changeIconE1(isHovering) {
+    var icon = document.getElementById('emailIcon1');
+    if (isHovering) {
+        icon.src = 'envelope-open.svg'; // This is the hover icon
+    } else {
+        icon.src = 'envelope-simple.svg'; // This is the original icon
+    }
+}
+
+function changeIconE2(isHovering) {
+    var icon = document.getElementById('emailIcon2');
+    if (isHovering) {
+        icon.src = 'envelope-open-fill.svg'; // This is the hover icon
+    } else {
+        icon.src = 'envelope-simple-fill.svg'; // This is the original icon
+    }
+}
